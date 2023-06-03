37140192bf9eedd8efe0f50caa3691620047dc1a
unknown <mateoloavvio12@gmail.com>
2023-06-03 15:41:55 +0200

    se termina el proyecto

--- API.js
+++ API.js
@@ -1,18 +1,23 @@
-import express from 'express';
-import { rutas } from './routes/rutas.js';
+import express from "express";
+import { rutas } from "./routes/rutas.js";
+import { establecerConexion } from "./database/conexion.js";
 
 export class API {
-
-    constructor() {
-        this.app = express()
-        this.enrutarPeticiones()
-    }
-    levantarServidor() {
-        this.app.listen(process.env.PORT, () => console.log(`encendido en ${process.env.PORT}`))
-    }
-    enrutarPeticiones() {
-        this.app.use('/', rutas)
-    }
-    conectarConBD() { }
-
+  constructor() {
+    this.app = express();
+    this.conectarConBD();
+    this.enrutarPeticiones();
+  }
+  levantarServidor() {
+    this.app.listen(process.env.PORT, () =>
+      console.log(`encendido en ${process.env.PORT}`)
+    );
+  }
+  enrutarPeticiones() {
+    this.app.use(express.json());
+    this.app.use("/", rutas);
+  }
+  conectarConBD() {
+    establecerConexion();
+  }
 }
--- routes/rutas.js
+++ routes/rutas.js
@@ -1,44 +1,28 @@
 import express from "express";
+import { ControladorHabitaciones } from "../controllers/ControladorHabitaciones.js";
+import { ControladorReservas } from "../controllers/ControladorReservas.js";
+
+let controladorHabitacion = new ControladorHabitaciones();
+let controladorReservas = new ControladorReservas()
 
 // voy a separar y personalizar las rutas de cada servicio del api rest
 
-export let rutas = express.Router()
+export let rutas = express.Router();
 
-rutas.get('/buscarhabitaciones', function (req, res) {
-    res.send('estoy buscando todas las habitaciones')
-})
+rutas.get("/buscarhabitaciones",controladorHabitacion.buscandoHabitaciones);
 
-rutas.get('/buscarhabitacion', function (req, res) {
-    res.send('estoy buscando una habitacion')
-})
+rutas.get("/buscarhabitacion/:idhabitacion",controladorHabitacion.buscandoHabitacion);
 
-rutas.post('/registrarhabitacion', function (req, res) {
-    res.send('estoy registrando una habitacion')
-})
+rutas.post("/registrarhabitacion",controladorHabitacion.registrandoHabitacion);
 
-rutas.put('/editandohabitacion', function (req, res) {
-    res.send('estoy actualizando una habitacion')
-})
+rutas.put("/editandohabitacion/:idhabitacion",controladorHabitacion.editandoHabitacion);
 
+rutas.get("/buscarreservas",controladorReservas.buscandoReservas);
 
+rutas.get("/buscarreserva/:idreserva",controladorReservas.buscandoReserva);
 
+rutas.post("/registrarreserva",controladorReservas.registrandoReservas);
 
-rutas.get('/buscarreservas', function (req, res) {
-    res.send('estoy buscando todas las reservas')
-})
+rutas.put("/editandoreserva/:idreserva",controladorReservas.editandoReserva);
 
-rutas.get('/buscarreserva', function (req, res) {
-    res.send('estoy buscando una reserva')
-})
-
-rutas.post('/registrarreserva', function (req, res) {
-    res.send('estoy registrando una reserva')
-})
-
-rutas.put('/editandoreserva', function (req, res) {
-    res.send('estoy actualizando una reserva')
-})
-
-rutas.delete('/eliminandoreserva', function (req, res) {
-    res.send('estoy eliminando una reserva')
-})
+rutas.delete("/eliminandoreserva/:idreserva",controladorReservas.eliminandoReserva);
